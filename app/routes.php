cbdf6b0de5543d98da3fae3b3606788fe41714d7
Bubblehead333 <joshlyell@live.co.uk>
2019-10-20 00:39:51 +0100

    -Basic slim/twig framework in place -Basic directory layout for a website

---
+++ app/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+$app->get('/', function($request, $response){
+    return $this->view->render($response, 'index.twig');
+});
+
+$app->get('/media', function($request, $response){
+    return 'Media';
+});
